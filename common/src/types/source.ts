d8abfe1788e1a9f1057e0f6f9eaaa70826749386
Charles Lien <charleslien97@gmail.com>
2025-10-17 11:37:04 -0700

    add jsdoc for Source

--- common/src/types/source.ts
+++ common/src/types/source.ts
@@ -1,3 +1,10 @@
+/**
+ * Represents a source of data.
+ *
+ * By default, can be a value or a promise.
+ *
+ * In the case that the type is a not function itself, this can also be a function that returns the value or promise.
+ */
 export type Source<T> =
   | T
   | Promise<T>
